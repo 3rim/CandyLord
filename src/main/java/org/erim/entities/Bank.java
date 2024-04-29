cd1126861347f4ae242ad20baaf10ce8a569c42b
Erim Medi <erim.medi@materna.group>
2024-04-29 13:25:29 +0200

    implemented Bank.class

--- src/main/java/org/erim/entities/Bank.java
+++ src/main/java/org/erim/entities/Bank.java
@@ -1,5 +1,24 @@
 package org.erim.entities;
 
 public class Bank {
+    private int savings;
 
+    public Bank() {
+        this.savings = 0;
+    }
+
+    public int getSavings() {
+        return savings;
+    }
+
+    /**
+     * Deposit some money
+     * @param amount the amount to be deposited
+     */
+    public void deposit(int amount) {
+        if(amount < 0)
+            throw new IllegalArgumentException("Cannot deposit negative values ");
+
+        this.savings += amount;
+    }
 }
