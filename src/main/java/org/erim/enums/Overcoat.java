9731e94f32b693788e582baac86a443c6d9c51ea
Erim Medi <erim.medi@materna.group>
2024-04-29 12:59:56 +0200

    implemented Overcoat enum

--- src/main/java/org/erim/enums/Overcoat.java
+++ src/main/java/org/erim/enums/Overcoat.java
@@ -1,4 +1,17 @@
 package org.erim.enums;
 
 public enum Overcoat {
+    STANDARD(10),
+    DOUBLE(20),
+    TRIPLE(30),
+    DEALER(50);
+
+    private final int capacity;
+
+    Overcoat(int storingSize) {this.capacity = storingSize;}
+
+
+    public int getCapacity() {
+        return capacity;
+    }
 }
